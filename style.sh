ca00c6c05720b119bac7fe8b0fb649dce55bf426
বাংলার নবাব <73420645+mafia2020@users.noreply.github.com>
2020-10-31 20:29:50 +0600

    Update style.sh

--- style.sh
+++ style.sh
@@ -9,9 +9,9 @@ echo "command_not_found_handle() {
         /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
 }
 
-PS1='\033[1;91mroot@termux[\033[1;93m\W\033[1;91m]:
+PS1='\033[1;91mroot@sohel_rana[\033[1;93m\W\033[1;91m]:
 # \033[1;92m'" > bash.bashrc
-toilet -f big 'SIAM' -F gay | lolcat > motd
+toilet -f big 'SOHEL' -F gay | lolcat > motd
 cd
 cd /$HOME
 figlet -f big -c SIAM
